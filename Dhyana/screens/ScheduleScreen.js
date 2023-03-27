91fa78451d7464a30525c1e736e61f1f69d038e6
Hong Weng <wengh7930@gmail.com>
2023-03-27 22:13:54 +1300

    update package

--- Dhyana/screens/ScheduleScreen.js
+++ Dhyana/screens/ScheduleScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text,Dimensions} from 'react-native';
+
+const Dev_width = Dimensions.get('window').width;
+const Dev_height = Dimensions.get('window').height;
+
+import {AntDesign, Entypo, Feather} from 'react-native-vector-icons';
+import Slider from '@react-native-community/slider'
+import ProgressCircle from 'react-native-progress-circle'
 
 const ScheduleScreen = () => {
   return (
     <View>
-      <Text>ScheduleScreen</Text>
+      <Text>Music Screen</Text>
     </View>
   );
 };
