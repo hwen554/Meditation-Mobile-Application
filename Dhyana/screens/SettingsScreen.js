041e7d72ac7bfb9dae0f2521ae047a16bff6b4e9
Richard Liu <richardliux23@gmail.com>
2023-04-01 10:56:17 +0800

    Not finished the profile page yet.

--- Dhyana/screens/SettingsScreen.js
+++ Dhyana/screens/SettingsScreen.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScreenContainer } from 'react-native-screens';
 
+// const SettingsScreen = () =>{
+//   return (
+//     <SafeAreaView style={[ScreenContainer]}>
+//       <ScrollView>
+//         <View>
+//           <Text>User</Text>
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   )
+// }
+
+// export default SettingsScreen;
 const SettingsScreen = () => {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>SettingsScreen</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
