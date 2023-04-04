eba26e07ab55c510b13d7d206950c1bc49bbcd6b
isabelxyz <izzzyz666@gmail.com>
2023-04-04 16:22:23 +1200

    update

--- Dhyana/screens/SettingsScreen.js
+++ Dhyana/screens/SettingsScreen.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const SettingsScreen = () => {
   return (
     <View>
-      <Text>SettingsScreen</Text>
+      <Text>SettingsScreen123</Text>
     </View>
   );
 };
