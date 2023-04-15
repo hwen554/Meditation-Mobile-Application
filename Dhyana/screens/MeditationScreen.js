70db1612ac641e351e29afe4450d3c0e459345a7
isabelxyz <izzzyz666@gmail.com>
2023-04-15 13:12:41 +1200

    Create MeditationScreen.js

---
+++ Dhyana/screens/MeditationScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MeditationScreen = () => {
+  return (
+    <View>
+        
+      <Text>MeditationScreen</Text>
+
+    </View>
+  )
+}
+
+export default MeditationScreen
+
+const styles = StyleSheet.create({})
