e9f03e7f0e37a7a2e36531b0bdee2756d2b2b1e0
Hong Weng <wengh7930@gmail.com>
2023-04-26 11:42:50 +1200

    Update Navigator.js

--- Dhyana/navigator/Navigator.js
+++ Dhyana/navigator/Navigator.js
@@ -79,7 +79,7 @@ const BottomTabNavigator = () => {
           const labels = {
             Schedule: 'Main',
             AllExercises: 'Search',
-            Settings: 'Profilo',
+            Settings: 'Protfilo',
           };
 
           return (
