0156e3df6478b675ca5e9f86fb3f23c17c2aef56
Hong Weng <wengh7930@gmail.com>
2023-05-10 10:48:26 +1200

    Revert "Merge branch 'Dhyana-Dev' of https://github.com/Compsci-Capstone-T2/T2-Project into Dhyana-Dev"
    This reverts commit eed8778da6667ee4e2f57862b048a397b68fd3d4, reversing
    changes made to 66dd73797ec0ee2b5cdb1788a65d471a5fb36e24.

--- Dhyana/App.js
+++ Dhyana/App.js
@@ -2,41 +2,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomTabNavigator from './navigator/Navigator';
-import CountContext from './screens/CountContext';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SettingsScreen } from './screens';
-import LoginScreen from './screens';
-import RegisterScreen from './screens';
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <BottomTabNavigator />
-//     </NavigationContainer>
-//   );
-// };
 
 const App = () => {
-  const [dailyCount, setDailyCount] = React.useState(0);
-  const [monthlyCount, setMonthlyCount] = React.useState(0);
-
-  const incrementCount = () => {
-    setDailyCount(dailyCount + 1);
-    setMonthlyCount(monthlyCount + 1);
-  };
-
   return (
-    <CountContext.Provider
-      value={{
-        dailyCount,
-        monthlyCount,
-        incrementCount,
-      }}
-    >
-      <NavigationContainer>
-        <BottomTabNavigator />
-      </NavigationContainer>
-    </CountContext.Provider>
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
   );
 };
 
--- Dhyana/firebase.js
+++ Dhyana/firebase.js
@@ -1,9 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getStorage, ref } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,9 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-
-const db = getFirestore();
-
-export {auth, db};
+export const storage = getStorage();
