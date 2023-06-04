cedae742405bd8fefe7e224de1f1868afeb0cd7c
Hong Weng <wengh7930@gmail.com>
2023-06-05 01:16:47 +1200

    update image

--- Dhyana/assets/index.ts
+++ Dhyana/assets/index.ts
@@ -1,10 +1,10 @@
 const AppImages = {
-    care_image: require('../assets/Images/care.png'),
-    mood_dairy_image:require('../assets/Images/mood.png'),
-    relax_image: require('../assets/Images/relax.png'),
+    care_image: require('../assets/Images/fi.jpg'),
+    mood_dairy_image:require('../assets/Images/newd.jpg'),
+    relax_image: require('../assets/Images/neww.jpg'),
     introduction_image:require('../assets/Images/intro.png'),
-    welcome: require('../assets/Images/welcome.png'),
-    clear_image:require('../assets/Images/newon.png')
+    welcome: require('../assets/Images/newr.jpg'),
+    clear_image:require('../assets/Images/newj.jpg')
 }
 
 export { AppImages };
